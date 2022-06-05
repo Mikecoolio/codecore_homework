9d5e7c302b000bc55c833d8f941ab4f1d8b6d636
mikecoolio <mikepoon604@gmail.com>
2022-06-05 12:33:11 -0700

    Added drawTopBorder, drawMiddleBorder, drawBottomBorder functions

--- boxIt.js
+++ boxIt.js
@@ -15,3 +15,53 @@ function drawLine(num) {
 
 console.log("horizontal lines", drawLine(8))
 
+/* 
+Write three functions: drawTopBorder, drawMiddleBorder and drawBottomBorder. 
+Each function should take a number, return a line of length includingcorner pieces. 
+You can make use of drawLine to implement these functions.
+
+
+
+Example usage:
+
+
+
+drawTopBorder(4) // returns '┏━━━━┓'
+drawTopBorder(0) // returns '┏┓'
+
+
+drawMiddleBorder(8) // returns '┣━━━━━━━━━┫'
+drawMiddleBorder(0) // returns '┣┫'
+
+
+drawBottomBorder(2) // returns '┗━━┛' 
+*/
+
+function drawTopBorder(num) {
+    let finishedTopBorder = ''
+    if (num === 0) {
+        return finishedTopBorder += '\u250F' + '\u2513'
+    } else {
+         return finishedTopBorder += '\u250F' + drawLine(num) + '\u2513'
+    }
+}
+
+console.log("top border", drawTopBorder(4))
+
+function drawMiddleBorder(num) {
+    let finishedMiddleBorder = ''
+    
+}
+
+console.log('middle border', drawMiddleBorder)
+
+function drawBottomBorder(num) {
+    let finishedBottomBorder = ''
+    if (num === 0) {
+        return finishedBottomBorder += '\u2517' + '\u251B'
+    } else {
+         return finishedBottomBorder += '\u2517' + drawLine(num) + '\u251B'
+    }
+}
+
+console.log("bottom border", drawBottomBorder(2))

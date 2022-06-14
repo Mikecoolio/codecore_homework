511cf0711f9ac1bb1172a497846ac4cb9163db5a
mikecoolio <mikepoon604@gmail.com>
2022-06-13 19:58:06 -0700

    added forward() function

--- turtle_graphics.js
+++ turtle_graphics.js
@@ -10,9 +10,13 @@ class Turtle {
         return this
     }
 
-    forward() {
-
+    forward(steps) {
+        console.log(`moved ${steps} along the X axis.  
+        Turtle moved starting from position ${this.x} on the X axis, ${steps} steps to the right along the X axis.
+        The current position for the Turtle on the X axis is ${this.x + steps}`)
+        return this
     }
 }
 
-let newTurtle = new Turtle(0, 0)
+//let newTurtle = new Turtle(0, 0)
+let moveForward = new Turtle(0, 0).forward(3)

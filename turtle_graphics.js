3a6910a8600c9018fb185c862f3340fbb9a05fcb
mikecoolio <mikepoon604@gmail.com>
2022-06-17 12:55:39 -0700

    created 2 functions to find the max number for x and y arrays, (for 2d arrays with x and y arrays within)

--- turtle_graphics.js
+++ turtle_graphics.js
@@ -9,7 +9,8 @@ class Turtle {
         this.allPositions = [[this.x, this.y]] // Array must be initialized in the contructor, for the point pair arrays to be added persistently
     }
 
-    compass() { // SWITCH STATEMENT FOR COMPASS NOT NEEDED, JUST POINT TO this.currentDirection
+    compass() { // SWITCH STATEMENT FOR COMPASS NOT NEEDED, JUST POINT TO this.currentDirection because all this does is point to the
+        //         default direction that the turtle is facing at the start of the program.
         return this.direction
     }
 
@@ -20,7 +21,7 @@ class Turtle {
         let currentDirection = this.compass()
         // console.log("the current direction is: ", currentDirection)
 
-        for (let i = 1; i <= steps; i++) {
+        for (let i = 0; i < steps; i++) { // i=0; i<steps is the same thing as i=1; i<=steps
             if (currentDirection === 'right') { // Must add to move forward, maybe minus to move backward?
                 this.x += 1;
             } else if (currentDirection === 'left') { // subtracting when movement is left means minusing x (reverse of 'right')
@@ -36,7 +37,7 @@ class Turtle {
             this.allPositions.push(recordPositions)
         }
         // SWITCH STATEMENT DOES NOT WORK INSIDE LOOPS, IT EVALUATES THE FIRST CASE THAT MATCHES THE VALUE OF THE VARIABLE THAT IS IN THE SWITCH STATEMENT
-    
+
         return this
     }
 
@@ -83,12 +84,59 @@ class Turtle {
     positions() {
         return this.allPositions 
     }
+
+    print() {
+        maxNum  = findMax(arrOfArrs)
+        // xMaxNum = findMaxNumber(xArr)
+        // yMaxNum = findMaxNumber(yArr)
+
+    }
 }
 
-// let move = new Turtle(0, 0).forward(3).right().forward(2);
-let move = new Turtle(0, 4).forward(3).left().forward(3);
-// console.log(move)
-console.log("allPositions: ", move.positions())
+let arrOfArrs = [[ 0, 4 ], [ 1, 4 ], [ 2, 4 ], [ 3, 4 ], [ 3, 5 ], [ 3, 6 ],
+[ 3, 7 ]]
+
+console.log("arrOfArrs length: ", arrOfArrs.flat())
+
+function findMax() {
+    let xCoord = []
+    let yCoord = []
+
+    arrOfArrs = arrOfArrs.flat()
+    for (let i = 0; i < arrOfArrs.length; i++) {
+        if (i % 2 === 0) {
+            let poppedOddIndexs = arrOfArrs.pop(arrOfArrs[i])
+            yCoord.push(poppedOddIndexs)
+        } else {
+            let poppedEvenIndexs = arrOfArrs.pop(arrOfArrs[i])
+            xCoord.push(poppedEvenIndexs)
+        }
+    }
+    console.log("ARROFAARRRS", arrOfArrs)
+    console.log("xCoord: ", xCoord)
+    console.log("yCoord: ", yCoord)
+
+    console.log("max number from left side: ", findMaxNumber(xCoord))
+    console.log("max number from right side :", findMaxNumber(yCoord))
+}
+
+
+function findMaxNumber(arr) {
+    maxNum = arr[0]
+    for (i = 1; i < arr.length; i++) {
+        if (arr[i] > maxNum) {
+            maxNum = arr[i]
+        }
+    }
+    return maxNum
+}
+
+findMax()
+
+// // let move = new Turtle(0, 0).forward(3).right().forward(2);
+// let move = new Turtle(0, 4).forward(3).left().forward(3);
+// // console.log(move)
+// console.log("allPositions: ", move.positions())
 
 
 // MAYBE CREATE 10 X 10 GRID FIRST

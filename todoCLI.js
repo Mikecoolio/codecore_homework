da794f1cf7c521108b7c0e0a60b93943c9caf449
mikecoolio <mikepoon604@gmail.com>
2022-06-23 18:38:54 -0700

    Wrote the 'View' with placeholder values for tasks

--- todoCLI.js
+++ todoCLI.js
@@ -1,14 +1,26 @@
 /*
-Write an interactive CLI todo list application using Node's readline and fs modules. The following describes what each action does. It would be best to implement each action as its own function.
-
-The Menu
-When the todoCLI.js is first executed, a menu as shown below should be displayed. These are all the options the user should be able to perform.
+View
+From the Todo Menu, pressing v then Enter should display the contents of the todo list then the Todo Menu again. See the example below.
 
 $ node todoCLI.js
 
 Welcome to Todo CLI!
 
 --------------------
+
+(v) View • ( n ) New • (cX) Complete • (dX) Delete • (q) Quit
+
+> v
+
+0 [✓] Take out the trash
+
+1 [✓] Buy toothpaste
+
+2 [ ] Buy Snickerdoodles
+
+3 [ ] Fix the climate
+
+4 [ ] Find a cure for aging
 
 (v) View • ( n ) New • (cX) Complete • (dX) Delete • (q) Quit
 
@@ -23,26 +35,53 @@ const readLine = require('readline').createInterface({
   });
 const selectors = ['(v)', '(n)', '(cX)', '(dX)', '(q)']
 const descriptors = ['View', 'New', 'Complete', 'Delete', 'Quit']
+const tasks = ['task1', 'task2', 'task3', 'task4']
+const emptyCheckbox = '[ ]'
+const fullCheckbox = '[\u2713]'
 
 function output() {
     console.log("Welcome to ToDo CLI!")
-    //process.stdout.write("Welcome to ToDo CLI!")
     console.log("-------------------------")
 }
 
-output()
 
-function menuComponents(selectors, descriptors) {
+function baseMenu(selectors, descriptors) {
     for (s = 0, d =0; s < selectors.length, d < descriptors.length; s++, d++) {
         process.stdout.write(`${selectors[s]}${descriptors[d]}` + " ")
     }
     console.log('\n')
     process.stdout.write('>')
     readLine.on('line', (input) => {
+        input = input.toLowerCase()
         console.log(`Received: ${input}`)
-        readLine.close()
+        if (input === 'v') {
+            displayTasks()
+        } else {
+             baseMenu(selectors, descriptors)
+        }
+        // readLine.close()
     })
 }
 
-menuComponents(selectors, descriptors)
+function displayTasks() {
+    const taskNumber = tasks.length
+    let checkboxOrNoCheckbox = '[ ]'
 
+    for (i = 0; i < tasks.length; i++) {
+        console.log(`${i} ${checkboxOrNoCheckbox} ${tasks[i]}`)
+        console.log('\n')
+    }
+    baseMenu(selectors, descriptors)
+}
+
+// function fillCheckMark() {
+
+// }
+
+// function checkIfTaskComplete() {
+
+// }
+
+output()
+baseMenu(selectors, descriptors)
+// displayTasks()

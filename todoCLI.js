095a789a1e9b7a7fc1ad1cd02cf80600ceaff29c
mikecoolio <mikepoon604@gmail.com>
2022-07-03 21:57:06 -0700

    fixed bug in deleteTask(), made minor adjustments, now will remove all needless comments

--- todoCLI.js
+++ todoCLI.js
@@ -7,45 +7,46 @@ const rl = readLine.createInterface({
     input: process.stdin,
     output: process.stdout
   });
-const phrases = []
-
 function output() {
     console.log("Welcome to ToDo CLI!")
     console.log("-------------------------")
 }
 
-function baseMenu(taskList = [], phrases, selectors = ['(v)', '(n)', '(cX)', '(dX)', '(q)'], descriptors = ['View', 'New', 'Complete', 'Delete', 'Quit']) {
-    console.log('phrases inside baseMenu', phrases)
-    console.log("RECIEVED TASKS LIST", taskList)
-    for (s = 0, d =0; s < selectors.length, d < descriptors.length; s++, d++) {
-        process.stdout.write(`${selectors[s]}${descriptors[d]}` + " ")
-    }
+// function baseMenu(taskList = [], phrases, selectors = ['(v)', '(n)', '(cX)', '(dX)', '(q)'], descriptors = ['View', 'New', 'Complete', 'Delete', 'Quit']) {
+function baseMenu(taskList = [], phrases) {
+    // console.log('phrases inside baseMenu', phrases)
+    // console.log("RECIEVED TASKS LIST", taskList)
+    // for (s = 0, d =0; s < selectors.length, d < descriptors.length; s++, d++) {
+    //     let line =
+    //     process.stdout.write(`${selectors[s]}${descriptors[d]}` + " " + "•" + " ")
+    // }
+    console.log('(v) View • ( n ) New • (cX) Complete • (dX) Delete • (q) Quit')
     console.log('\n')
     rl.question('>', input => {
         let cleanedInput = input.toString().toLowerCase().trim()
 
         if (cleanedInput === 'v') {
-            console.log(`inside v, recieved input: ${cleanedInput}`)
-            console.log('v is recieving from phrases inide baseMenu', phrases)
+            // console.log(`inside v, recieved input: ${cleanedInput}`)
+            // console.log('v is recieving from phrases inide baseMenu', phrases)
             if (phrases === undefined) {
                 // displayTasks(phrases)
                 displayTasksWithoutUpdating(phrases)
             } else if (phrases != undefined){
                 // displayTasksWithoutUpdating(phrases)
-                displayTasks([], phrases)
+                displayTasksV(phrases.sort())
             }
         } else if (cleanedInput === 'n') {
             // console.log("inside n")
             newTask();
         } else if (cleanedInput.includes('c')) {
-            console.log('inside c')
-            console.log("taskList inside baseMenu", taskList)
+            // console.log('inside c')
+            // console.log("taskList inside baseMenu", taskList)
             checkTasks(cleanedInput, taskList, phrases)
             // need to code a checker function that checks if the number attached beside c exceeds the length of the taskList index or not
         } else if (cleanedInput.includes('d')) {
-            deleteTask(cleanedInput, taskList)
+            deleteTask(cleanedInput, taskList, phrases.sort())
         } else if (cleanedInput === 'q') {
-            // console.log("See you soon! 😄")
+            console.log("See you soon! 😄")
             rl.close()
         }
         else {
@@ -55,27 +56,35 @@ function baseMenu(taskList = [], phrases, selectors = ['(v)', '(n)', '(cX)', '(d
     })
 }
 
-function deleteTask(input, taskList) {
+function deleteTask(input, taskList, phrases) {
+    // console.log("INSIDE deleteTask phrases", phrases)
+    // console.log("input inside deleteTask", input)
+    // console.log("taskList inside deleteTask", taskList)
+    // console.log("phrases inside deleteTask", phrases)
+
     let splittedInput = input.split('')
     let desiredIndexOfTaskToBeUpdated = parseInt(splittedInput[1])
+    // console.log("desiredIndexOfTaskToBeUpdated inside deleteTask", desiredIndexOfTaskToBeUpdated)
 
     if (taskList.length > desiredIndexOfTaskToBeUpdated) {
-        updatedDeletedTask = taskList.splice(desiredIndexOfTaskToBeUpdated, 1)
-        // console.log(`Deleted ${updatedDeletedTask}}`)
+        let updatedDeletedTask = taskList.splice(desiredIndexOfTaskToBeUpdated, 1)
+        phrases.splice(desiredIndexOfTaskToBeUpdated, 1)
+
+        console.log(`Deleted ${updatedDeletedTask} \n`)
         // console.log("taskList after deleting desired task", taskList)
-        baseMenu(taskList)
+        baseMenu(taskList, phrases)
     } else {
-        baseMenu(taskList)
+        baseMenu(taskList, phrases)
     }
 }
 
 function checkTasks(input, taskList, phrases) {
     let splittedInput = input.split('')
     let desiredIndexOfTaskToBeUpdated = parseInt(splittedInput[1])
-    console.log('phrases inside checkTasks', phrases)
-    console.log("splittedInput inside checkTasks", splittedInput)
-    console.log("desiredIndexOfTaskToBeUpdated inside checkTasks", desiredIndexOfTaskToBeUpdated)
-    console.log("taskList inside checkTasks", taskList)
+    // console.log('phrases inside checkTasks', phrases)
+    // console.log("splittedInput inside checkTasks", splittedInput)
+    // console.log("desiredIndexOfTaskToBeUpdated inside checkTasks", desiredIndexOfTaskToBeUpdated)
+    // console.log("taskList inside checkTasks", taskList)
 
     if (taskList.length > desiredIndexOfTaskToBeUpdated) {
         // console.log("desiredIndexOfTaskToBeUpdated", desiredIndexOfTaskToBeUpdated)
@@ -96,43 +105,83 @@ function checkTasks(input, taskList, phrases) {
 }
 
 function displayTasksWithoutUpdating(phrases) {
-    console.log("INSIDE displayTasksWithoutUpdating")
+    // console.log("INSIDE displayTasksWithoutUpdating")
     let unfinishedTasks = []
     
-    if (phrases === undefined) {
-        for (let task in tasks) {
-            let singlePhrase = `${task} ${emptyCheckbox} ${tasks[task]} \n`
-            unfinishedTasks.push(singlePhrase)
-            console.log(singlePhrase)
-        }
-        baseMenu(tasks, unfinishedTasks)
+    if (tasks.length === 0) {
+        console.log('')
+        console.log("List is empty...\n")
+        baseMenu()
     } else {
-        for (let phrase in phrases) {
-            console.log(phrases[phrase])
+        if (phrases === undefined) {
+            for (let task in tasks) {
+                let singlePhrase = `${task} ${emptyCheckbox} ${tasks[task]} \n`
+                unfinishedTasks.push(singlePhrase)
+                console.log(singlePhrase)
+            }
+            baseMenu(tasks, unfinishedTasks)
+        } else {
+            for (let phrase in phrases) {
+                console.log(phrases[phrase])
+            }
+            baseMenu(tasks, phrases)
         }
-        baseMenu(tasks, phrases)
     }
 }
 
+function displayTasksV(phrases) {
+    // console.log('INSIDE displayTasksV (phrases)', phrases)
+    // console.log(typeof phrases)
+    
+    for (let phrase in phrases) {
+        console.log(phrases[phrase])
+    }
+    baseMenu(tasks, phrases)
+    // if (phrases === undefined) {
+    //     baseMenu(tasks)
+    // } else if (phrases != undefined) {
+    //     for (i=0; i<phrases; i++) {
+    //         console.log(phrases[i])
+    //     }
+    //     baseMenu(tasks, phrases)
+    // }
+    // if (phrases.length > 0) {
+    //     for (i=0; i<phrases; i++) {
+    //         console.log(phrases[i])
+    //     }
+    //     baseMenu(tasks, phrases)
+    // }
+}
+
 function displayTasks(update = [], phrases) { 
-    console.log('INSIDE displayTasks')
+    // console.log('INSIDE displayTasks')
     let indexOfUpdatedPhrase = update[1]
     let updatedTask = update[0]
     let updatedPhrase = `${indexOfUpdatedPhrase} ${fullCheckBox} ${updatedTask} \n` 
     let listOfTasksWithCheckbox = []
     let phrasesArray = []
-    console.log('phrases inside displayTasks', phrases)
-    console.log('listOfTasksWithCheckbox inside displayTasks before conditional', listOfTasksWithCheckbox)
-    console.log("update inside displayTasks", update)
-    console.log("updatedPhrase", updatedPhrase)
-    console.log("indexOfUpdatedPhrase", typeof indexOfUpdatedPhrase)
-    console.log("tasks.length inside displayTasks()", tasks.length)
+    // console.log('phrases inside displayTasks', phrases)
+    // console.log('listOfTasksWithCheckbox inside displayTasks before conditional', listOfTasksWithCheckbox)
+    // console.log("update inside displayTasks", update)
+    // console.log("updatedPhrase", updatedPhrase)
+    // console.log("indexOfUpdatedPhrase", typeof indexOfUpdatedPhrase)
+    // console.log("tasks.length inside displayTasks()", tasks.length)
 
-    if (tasks.length === 0) {
-        console.log('')
-        console.log("List is empty...\n")
-        baseMenu()
-    } else if (tasks.length > 0) {
+    // if (tasks.length === 0) {
+    //     console.log('')
+    //     console.log("List is empty...\n")
+    //     baseMenu()
+    // } else 
+
+    // if (phrases.length != undefined) {
+    //     if (phrases.length > 0) {
+    //         for (i=0; i<phrases; i++) {
+    //             console.log(phrases[i])
+    //         }
+    //     }    
+    // }
+
+    if (tasks.length > 0) {
         for (i = 0; i < tasks.length; i++) {  
             console.log('')
             let phrase = `${i} ${emptyCheckbox} ${tasks[i]} \n`
@@ -152,8 +201,8 @@ function displayTasks(update = [], phrases) {
             phrases.indexOf(task) < 0
         }))
     }
-    console.log("phrasesArray inside displayTasks before sending to combineArrays", phrasesArray)
-    console.log("listOfTasksWithCheckbox inside displayTasks before sending to combineArrays", listOfTasksWithCheckbox)
+    // console.log("phrasesArray inside displayTasks before sending to combineArrays", phrasesArray)
+    // console.log("listOfTasksWithCheckbox inside displayTasks before sending to combineArrays", listOfTasksWithCheckbox)
 
     if (phrasesArray[0] === listOfTasksWithCheckbox[0]) {
         for (i=0; i<phrasesArray.length; i++) {
@@ -163,29 +212,19 @@ function displayTasks(update = [], phrases) {
         baseMenu(tasks, phrasesArray)
     } else {
         if (tasks[indexOfUpdatedPhrase] != undefined) {
-            console.log(`Completed ${tasks[indexOfUpdatedPhrase]}`)
+            console.log(`Completed ${tasks[indexOfUpdatedPhrase]}\n`)
             combineArrays(phrasesArray, listOfTasksWithCheckbox)
         } else {
-            combineArrays(phrasesArray, listOfTasksWithCheckbox)
+            console.log(`Completed ${tasks[indexOfUpdatedPhrase]}\n`)
+            // combineArrays(phrasesArray, listOfTasksWithCheckbox)
         }
     }
-
-    // combineArrays(phrasesArray, listOfTasksWithCheckbox)
-
-    // let combinedArrays =
-    // combineArrays(phrasesArray, listOfTasksWithCheckbox)
-    // console.log("output of combineArrays() inside displayTasks()", combinedArrays)
-        // listOfTasksWithCheckbox[indexOfUpdatedPhrase] = updatedPhrase
-        // let joined = listOfTasksWithCheckbox.join('')
-        // console.log("joined:", joined)
-        //console.log(completedTasks.join(''))
-        // baseMenu(tasks, listOfTasksWithCheckbox)
     }
 }
 
 function combineArrays(phrasesArray, listOfTasksWithCheckbox) {
-    let unfinishedTasks = []
-    console.log('INSIDE combineArrays')
+    // let unfinishedTasks = []
+    // console.log('INSIDE combineArrays')
     // console.log("phrasesArray inside combineArrays: ", phrasesArray)
     // console.log("listOfTasksWithCheckbox inside combineArrays: ", listOfTasksWithCheckbox)
 
@@ -212,25 +251,21 @@ function combineArrays(phrasesArray, listOfTasksWithCheckbox) {
     }
 
     let finalArr = phrasesArray.concat(listOfTasksWithCheckbox)
-
-    // for (i=0; i<finalArr.length; i++) {
-    //     console.log(finalArr[i])
-    // }
-    console.log("finalArr inside combineArrays: ", finalArr)
+    // console.log("finalArr inside combineArrays: ", finalArr)
     
     let uniqueTasks = [...new Set(finalArr)]
-    console.log("uniqueTasks inside combineArrays", uniqueTasks)
+    // console.log("uniqueTasks inside combineArrays", uniqueTasks)
 
-    if (uniqueTasks != undefined) {
-        // for (let task in tasks) {
-        //     let singlePhrase = `${task} ${emptyCheckbox} ${tasks[task]} \n`
-        //     unfinishedTasks.push(singlePhrase)
-        //     console.log(singlePhrase)
-        // }
-        for (i=0 ; i<uniqueTasks.length; i++) {
-            console.log(uniqueTasks[i])
-        }
-    }
+    // if (uniqueTasks != undefined) {
+    //     // for (let task in tasks) {
+    //     //     let singlePhrase = `${task} ${emptyCheckbox} ${tasks[task]} \n`
+    //     //     unfinishedTasks.push(singlePhrase)
+    //     //     console.log(singlePhrase)
+    //     // }
+    //     // for (i=0 ; i<uniqueTasks.length; i++) {
+    //     //     console.log(uniqueTasks[i])
+    //     // }
+    // }
 
     baseMenu(tasks, uniqueTasks)
 }

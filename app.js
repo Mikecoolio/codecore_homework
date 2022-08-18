4e27993829700737b959020bb410d3276b9e0334
mikecoolio <mikepoon604@gmail.com>
2022-08-18 14:25:59 -0700

    turned text element letters into buttons

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ alphabet = alphabet.toUpperCase()
 alphabet = alphabet.split('')
 console.log(alphabet.length)
 
-a = document.querySelectorAll('#root')
+// a = document.querySelectorAll('#root')
 
 for (i=0;i<alphabet.length;i++) {
-    document.body.appendChild(document.createElement('div'))
-    letter = document.body.appendChild(document.createTextNode(alphabet[i]))
+    document.body.appendChild(document.createElement('button')).innerText = alphabet[i].toUpperCase()
 }
 
+const letter_divs = document.getElementsByTagName('section')
+
+for (i=0;i<letter_divs.length;i++) {
+    letter_divs[i].className = "letter"
+    console.log(letter_divs[i])
+}
 

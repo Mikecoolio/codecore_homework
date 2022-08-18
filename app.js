fa578b129a36ece0281bc9d3082b05a35b329002
mikecoolio <mikepoon604@gmail.com>
2022-08-17 23:26:13 -0700

    can see the alphabet on the page in its own divs, need to see ohow can i add a class to all these letter divs

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-a..z
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+alphabet = alphabet.toUpperCase()
+alphabet = alphabet.split('')
+console.log(alphabet.length)
+
+a = document.querySelectorAll('#root')
+
+for (i=0;i<alphabet.length;i++) {
+    document.body.appendChild(document.createElement('div'))
+    letter = document.body.appendChild(document.createTextNode(alphabet[i]))
+}
+
+

4215fd0eac7e57518d6abf13310c17386737a709
mikecoolio <mikepoon604@gmail.com>
2022-08-30 12:50:10 -0700

    got the index/indices of the guessed letter into an array:

--- app.js
+++ app.js
@@ -33,25 +33,19 @@ buttons.forEach(button => {
 });
 
 // Mkae buttons clickable
+let spans = document.getElementsByTagName('span')
+for (i=0;i<spans.length;i++) {
+    spans[i].className = 'span'
+}
+
 const make_clickable = document.getElementsByClassName('button')
 for (i=0;i<make_clickable.length;i++) {
-    // console.log("make_clickable[i]", make_clickable[i].innerHTML)
     make_clickable[i].onclick = function(elem){
-        test = this.innerHTML.toLowerCase()
-        console.log(`Clicked ${test}`)
-        checkAnswer(test)
+        guess = this.innerHTML.toLowerCase()
+        console.log(`Clicked ${guess}`)
+        checkAnswer(guess)
     }
 }
-
-function get_elem_by_id() {
-    spans = document.getElementsByTagName('span')
-
-    for (i=0;i<spans.length;i++) {
-        let element = document.getElementById(`${i}`)//[i]
-        console.log("element", element.innerHTML)
-    }
-}
-get_elem_by_id()
 
 // Answer check
 function checkAnswer(guess) {
@@ -60,53 +54,36 @@ function checkAnswer(guess) {
     for(i=0; i<splitted_answer.length; i++) {
         console.log(splitted_answer[i]) 
         console.log("i", i)
-    }
-
         if(guess === splitted_answer[i]) {
             // answer_arr[i] = ''
+
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp
+            let findLetter = guess
+            let regexSearch = new RegExp(findLetter, 'gi')
+            indexOfGuessedLetterArr = []
+
+            // Grab the index/indices of the guessed letter
+            while(regexSearch.exec(answer)) {
+                indexOfGuessedLetterArr.push(regexSearch.lastIndex)
+            }
             console.log(`GUESSED RIGHT LETTER: ${splitted_answer[i]}`)
-            //  FIND FIRST SPAN ELEM WITH JUST A LINE AND PUT THE GUESS IN ITS PLACE
-            // let target_single_span_index = document.getElementsByTagName("span")
-            // let spanList = Object.values(target_single_span_index)
-
-            // spanList.find(elem => elem.innertext === i)
-
-            // spanList.find(element => {
-            //     element = "_"
-            //     console.log(`Element found: ${element}`)
-            //     element.innerHTML = guess
-            //     // element.innerHTML = "_"
-            //     // element.innerHTML.toLowerCase() === splitted_answer[i]
-            // })
-
-            // let target_single_span_index = document.getElementsByClassName('0')
-            // single_span = target_single_span_index[0]
+            spans[0].innerText = str
+            console.log("index of answer", answer.indexOf(guess))
         }
+        guessedLetterArrFixedIndices = indexOfGuessedLetterArr.map(index => index - 1)
+        console.log("index of guessed letter", guessedLetterArrFixedIndices)
     }
+}
 
-    // answer.split('').forEach(function(letter) {
-    //     if (letter === guess) {
-    //         console.log(`The Guessed letter: ${letter} was correct`)
-    //         spans = document.getElementsByTagName('span')
-    //         for(i=0; i<spans.length; i++) {
-                
-    //             span = spans[i]
-    //             span.innerHTML = letter
-    //             if (span === null) {
-    //                 console.log("NULL DETECTED AT", span)
-    //             }
-    //         }
-    //         // answer_arr.push(letter)
-    //         // return answer_arr
-    //     }
-    //     else {
-    //         console.log(`The guess: ${guess} is incorrect`)
-    //         livesChecker(1)
-    //     }
-       
-    // })
-    // console.log("answer_arr", answer_arr)
+function get_elem_by_id() {
+    let spans = document.getElementsByTagName('span')
 
+    for (i=0;i<spans.length;i++) {
+        let element = document.getElementById(`${i}`)//[i]
+        console.log("element", element.innerHTML)
+    }
+}
+get_elem_by_id()
 
 // Lives check
 function livesChecker(minus_a_life) {
